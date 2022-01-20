eb1c0a8829646c34da52c1c321afbb10212d27d2
unCanardSauvage <56954006+unCanardSauvage@users.noreply.github.com>
2022-01-20 21:43:49 +0100

    Add files via upload
    commitation

---
+++ PFE_ARTICO/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace PFE_ARTICO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.side_panel = new System.Windows.Forms.Panel();
+            this.play_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.userCtrl_panel = new System.Windows.Forms.Panel();
+            this.userControl_Jouer1 = new PFE_ARTICO.UserControl_Jouer(this);
+            this.userControl_Savoir1 = new PFE_ARTICO.UserControl_Savoir();
+            this.side_panel.SuspendLayout();
+            this.userCtrl_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // side_panel
+            // 
+            this.side_panel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.side_panel.Controls.Add(this.play_button);
+            this.side_panel.Controls.Add(this.button1);
+            this.side_panel.Controls.Add(this.exit_button);
+            this.side_panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.side_panel.Location = new System.Drawing.Point(0, 0);
+            this.side_panel.Name = "side_panel";
+            this.side_panel.Size = new System.Drawing.Size(130, 547);
+            this.side_panel.TabIndex = 0;
+            // 
+            // play_button
+            // 
+            this.play_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.play_button.Location = new System.Drawing.Point(4, 122);
+            this.play_button.Name = "play_button";
+            this.play_button.Size = new System.Drawing.Size(123, 46);
+            this.play_button.TabIndex = 3;
+            this.play_button.Text = "Jouer";
+            this.play_button.UseVisualStyleBackColor = true;
+            this.play_button.Click += new System.EventHandler(this.play_button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(4, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "En savoir plus";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exit_button
+            // 
+            this.exit_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit_button.Location = new System.Drawing.Point(4, 226);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(123, 46);
+            this.exit_button.TabIndex = 1;
+            this.exit_button.Text = "Quitter";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // userCtrl_panel
+            // 
+            this.userCtrl_panel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userCtrl_panel.Controls.Add(this.userControl_Savoir1);
+            this.userCtrl_panel.Controls.Add(this.userControl_Jouer1);
+            this.userCtrl_panel.Location = new System.Drawing.Point(129, 0);
+            this.userCtrl_panel.Name = "userCtrl_panel";
+            this.userCtrl_panel.Size = new System.Drawing.Size(818, 547);
+            this.userCtrl_panel.TabIndex = 1;
+            // 
+            // userControl_Jouer1
+            // 
+            this.userControl_Jouer1.Location = new System.Drawing.Point(0, 0);
+            this.userControl_Jouer1.Name = "userControl_Jouer1";
+            this.userControl_Jouer1.Size = new System.Drawing.Size(818, 547);
+            this.userControl_Jouer1.TabIndex = 0;
+            // 
+            // userControl_Savoir1
+            // 
+            this.userControl_Savoir1.Location = new System.Drawing.Point(0, 0);
+            this.userControl_Savoir1.Name = "userControl_Savoir1";
+            this.userControl_Savoir1.Size = new System.Drawing.Size(818, 547);
+            this.userControl_Savoir1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 547);
+            this.Controls.Add(this.userCtrl_panel);
+            this.Controls.Add(this.side_panel);
+            this.Name = "Form1";
+            this.side_panel.ResumeLayout(false);
+            this.userCtrl_panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel side_panel;
+        private System.Windows.Forms.Panel userCtrl_panel;
+        private System.Windows.Forms.Button play_button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exit_button;
+        private UserControl_Jouer userControl_Jouer1;
+        private UserControl_Savoir userControl_Savoir1;
+    }
+}
+
---
+++ PFE_ARTICO/Grid.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PFE_ARTICO
+{
+    class Grid
+    {
+        public int Groupe { get; set; }
+
+        // S'il y a une tache du même groupe, passe à true
+        public bool isHaveATask { get; set; }
+
+        // Où commence la partie de grille
+        public int Location { get; set; }
+
+        public Grid(Panel panel, int size, int groupe, int x)
+            : base()
+        {
+            Groupe = groupe;
+            for (int i = 0; i < size; i++)
+            {
+                if (i == size - 1)
+                {
+                    var myPanel = new Panel
+                    {
+                        Size = new System.Drawing.Size(20, 30),
+                        Location = new System.Drawing.Point(15 + 20 * i + x * 20, 30 + Groupe * 45),
+                        BackColor = Color.LightGray,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Visible = true,
+                        Enabled = true
+                    };
+                    myPanel.SendToBack();
+                    panel.Controls.Add(myPanel);
+                }
+                else
+                {
+                    var myPanel = new Panel
+                    {
+                        Size = new System.Drawing.Size(20, 30),
+                        Location = new System.Drawing.Point(15 + 20 * i + x * 20, 30 + Groupe * 45),
+                        BackColor = chooseColor(groupe),
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Visible = true,
+                        Enabled = true,
+                    };
+                    myPanel.SendToBack();
+                    panel.Controls.Add(myPanel);
+                }
+            }
+        }
+
+        private Color chooseColor(int nbr)
+        {
+            Color color;
+
+            switch (nbr)
+            {
+                case 0:
+                    color = Color.LightPink;
+                    break;
+                case 1:
+                    color = Color.LightBlue;
+                    break;
+                case 2:
+                    color = Color.MediumPurple;
+                    break;
+                case 3:
+                    color = Color.LightGreen;
+                    break;
+                case 4:
+                    color = Color.LightGoldenrodYellow;
+                    break;
+                case 5:
+                    color = Color.Transparent;
+                    break;
+                case 6:
+                    color = Color.LightSlateGray;
+                    break;
+                default:
+                    color = Color.LightCoral;
+                    break;
+            }
+
+            return color;
+        }
+
+
+    }
+}
